032156fc085bf596edbd4f44570f3b92c194707c
Alex Monteiro <alex.mecn@gmail.com>
2020-11-26 22:03:30 -0300

    first commit

---
+++ src/net/codejava/networking/MultipartFileUploader.java
@@ -0,0 +1,37 @@
+package net.codejava.networking;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+ 
+/**
+ * This program demonstrates a usage of the MultipartUtility class.
+ * @author www.codejava.net
+ *
+ */
+public class MultipartFileUploader {
+ 
+    public static void main(String[] args) {
+        String charset = "UTF-8";
+        File uploadFile1 = new File("/home/alex/Pictures/2020-09-22.jpg");
+        String requestURL = "https://localhost8080/teste";
+        String apiKey = "apiKey";
+        String userEmail = "userEmail";
+ 
+        try {
+            MultipartUtility multipart = new MultipartUtility(requestURL, charset, apiKey, userEmail);
+             
+            multipart.addFilePart("file", uploadFile1);
+ 
+            List<String> response = multipart.finish();
+             
+            System.out.println("SERVER REPLIED:");
+             
+            for (String line : response) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
